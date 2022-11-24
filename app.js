16a1fd550231cf37535a48450ad4746c480dad1f
Eric <menaericdaniel@gmail.com>
2022-11-24 11:30:59 -0300

    add routes & views

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const app = express();
 const port = 3030;
+const path = require('path')
 
-console.log('trabajando en express')
-
+/* rutas */
+app.get('/',(req,res) => res.sendFile(path.join(__dirname, 'views', 'home.html')));
+app.get('/login',(req,res) => res.sendFile(path.join(__dirname, 'views', 'login.html')));
+app.get('/registro',(req,res) => res.sendFile(path.join(__dirname, 'views', 'register.html')));
+app.get('/detalle',(req,res) => res.sendFile(path.join(__dirname, 'views','detalle-producto.html')));
+app.get('/carrito',(req,res) => res.sendFile(path.join(__dirname,'views','carrito.html')));
+app.get('*',(req,res) => res.sendFile(path.join(__dirname, 'views', '404.html')))
 
 
 app.listen(port, () => console.log(`Servidor levantado en el puerto ${port}`))
